6e85a5df8344c281d433944c909275cc620f398f
Sheraz <csheraz@hotmail.com>
2025-08-01 23:39:44 +0100

    1234

--- app/(auth)/forget-password/page.tsx
+++ app/(auth)/forget-password/page.tsx
@@ -1,7 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '@/app/ui/login-form';
-import { Suspense } from 'react';
- 
+
 export default function ForgetPassword() {
   return (
     <main className="flex items-center justify-center md:h-screen">
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,6 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '@/app/ui/login-form';
-import { Suspense } from 'react';
+
  
 export default function Register() {
   return (
