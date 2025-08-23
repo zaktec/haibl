2e52b7624fac77848e407c4dafc9b88a176b428d
Sheraz <csheraz@hotmail.com>
2025-08-23 23:11:06 +0100

    metachange

--- app/(root)/onboarding/page.tsx
+++ app/(root)/onboarding/page.tsx
@@ -1,5 +1,11 @@
 import MailForm from '@/app/ui/root/MailForm';
+import { Metadata } from 'next';
 
+
+export const metadata: Metadata = {
+  title: 'MTH - Onboarding',
+  description: 'Book your place in our Maths Revision Classes in Levenshulme, Manchester. GCSE Foundation, Higher and KS3 classes available starting October 2025.',
+};
 export default function OnboardingPage() {
     return (
         <main className="flex min-h-screen items-center justify-center">
