c2b5db5fbefc34db2145ee09d77be9821beee5f1
Sheraz <csheraz@hotmail.com>
2025-10-17 22:45:29 +0100

    configure RDS database with endpoint

--- app/seed/db.js
+++ app/seed/db.js
@@ -2,11 +2,11 @@ import postgres from 'postgres';
 
 export function getDb() {
   const sql = postgres({
-    host: process.env.RDS_HOST,
-    port: parseInt(process.env.RDS_PORT) || 5432,
-    database: process.env.RDS_DATABASE,
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT) || 5432,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     ssl: process.env.NODE_ENV === 'production' ? 'require' : false,
   });
   
